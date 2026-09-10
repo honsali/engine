d6805c1c379206eeb7c8422ce696beb56ef5d76e
honsali <honsali@gmail.com>
2026-09-10 10:47:06 +0100

    manual: change ViewFiltrerEmploye formating

--- src/main/java/modules/rh/employe/ViewFiltrerEmploye.java
+++ src/main/java/modules/rh/employe/ViewFiltrerEmploye.java
@@ -7,32 +7,27 @@ import modules.rh.RhModule;
 
 public class ViewFiltrerEmploye extends ViewComposer<Employe> {
 
-    public Component rootComponent() {
-        Employe e = entity(Employe.class);
-        FiltreEmploye filtre = new FiltreEmploye();
-        Component elementFiltre = element(filtre);
+  public Component rootComponent() {
+    Employe e = entity(Employe.class);
+    FiltreEmploye filtre = new FiltreEmploye();
+    Component elementFiltre = element(filtre);
 
-        return block()
-            .margin("20px 40px")
-            .content(
-                inColumn()
-                    .spans(16, 8)
-                    .column(
-                        section()
-                            .margin("0")
-                            .content(
-                                primaryPanel()
-                                    .title("listeEmploye")
-                                    .content(element(new TableauEmploye(filtre.action)))
-                            )
-                            .actionBlock(button(addAction(e, RhModule.pageCreerEmploye)))
-                    )
-                    .column(
-                        block()
-                            .margin("62px 0px")
-                            .content(elementFiltre)
-                    )
-            );
-    }
+    return //
+    block().margin("20px 40px").content(
+        inColumn().spans(16, 8)
+            .column(
+                section().margin("0")
+                    .content(
+                        primaryPanel().title("listeEmploye").content(
+                            element(new TableauEmploye(filtre.action))//
+                        ))
+                    .actionBlock(//
+                        button(addAction(e, RhModule.pageCreerEmploye))//
+                    ))//
+            .column(
+                block().margin("62px 0px").content(elementFiltre)//
+            )//
+    );
+  }
 
 }
